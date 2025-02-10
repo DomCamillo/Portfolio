4faddaa1142dda9e1d9a4ec21d86121d14349dff
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-02-10 16:28:48 +0100

    added header and above the fold section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { AboveTheFoldComponent } from '../../components/above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from '../../components/about-me/about-me.component';
+import { SkillsComponent } from '../../components/skills/skills.component';
+import { PortfolioComponent } from '../../components/portfolio/portfolio.component';
+import { ProjectsComponent } from '../../components/portfolio/projects/projects.component';
+import { CommentsComponent } from '../../components/comments/comments.component';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AboveTheFoldComponent, AboutMeComponent, 
+    SkillsComponent, PortfolioComponent, ProjectsComponent, 
+    CommentsComponent, ContactFormComponent, FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
