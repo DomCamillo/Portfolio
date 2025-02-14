318bb7206c2c4c12a246159b24cc4aaa4e659a04
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-02-14 11:17:51 +0100

    added animations/transition

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit,NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
 
+
+export class AboutMeComponent implements OnInit {
+  animateIcons = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.animateIcons = true; 
+    }, 500);
+  }
 }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
-export class SkillsComponent {
+export class SkillsComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {
+   
+  }
+  icon:any[]=[];
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            console.log(entry); 
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible'); 
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.2 }); 
 
+    const skillIcons = this.el.nativeElement.querySelectorAll('.icon-skills');
+    skillIcons.forEach((icon: Element) => {
+        console.log(icon);
+        observer.observe(icon);
+    });
 }
+}
+
