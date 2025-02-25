5591dee1dbb64472aafad005acf6ff58da5d5ff0
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-02-25 17:12:26 +0100

    impressum translate

--- src/app/pages/impressum/impressum.component.ts
+++ src/app/pages/impressum/impressum.component.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { RouterLink } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [FooterComponent,RouterLink],
+  imports: [FooterComponent,RouterLink, HeaderComponent],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
 export class ImpressumComponent {
+
+  constructor(private translate: TranslateService) {}
+
+
+  get verantwortlich(): string {
+    return this.translate.instant('impressum.verantwortlich');
+  }
+  get haftungsausschlussH1(): string {
+    return this.translate.instant('impressum.haftungsausschlussH1');
+  }
+  get haftungsausschlussText(): string {
+    return this.translate.instant('impressum.haftungsausschlussText');
+  }
+  get urheberrechtH1(): string {
+    return this.translate.instant('impressum.urheberrechtH1');
+  }
+  get urheberrechtText(): string {
+    return this.translate.instant('impressum.urheberrechtText');
+  }
+  get datenschutzH1(): string {
+    return this.translate.instant('impressum.datenschutzH1');
+  }
+  get datenschutz(): string {
+    return this.translate.instant('impressum.datenschutz');
+  }
+  get datenschutzLink(): string {
+    return this.translate.instant('impressum.datenschutzLink');
+  }
+  get goBack(): string {
+    return this.translate.instant('impressum.goBack');
+  }
+ 
+
+
  email = 'dominic@moerth.ch'
 }
--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+
+    constructor(private translate: TranslateService) {}
+
+
+
+
+
   email = 'dominic@moerth.ch'
 }
