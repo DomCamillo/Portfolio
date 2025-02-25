2d74bca8fb40b376fa81faf2bf6b97abe4a30837
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-02-25 16:23:19 +0100

    added few new animations / fixed font-size

--- src/app/components/above-the-fold/above-the-fold.component.ts
+++ src/app/components/above-the-fold/above-the-fold.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
+  constructor(private translate: TranslateService) {
 
+ this.getCurrentLanguage();
+   
+      
+    
+  }
+
+
+
+  getCurrentLanguage() : string{
+  return this.translate.currentLang;
+  }
+
+  get iamText(): string {
+    return this.translate.instant('aboveTheFold.iam');
+  }
 
   openGit(){
     window.open( "https://github.com/DomCamillo", "_blank");
   }
+
+ 
 
   scrollToContact(){
     
